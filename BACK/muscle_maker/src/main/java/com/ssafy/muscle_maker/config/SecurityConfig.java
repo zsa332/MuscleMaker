04c67e97527c87b931dfbe2b639d660e8ecf50ac
youngseo <tim9603@naver.com>
2024-01-19 11:06:47 +0900

    solved conflict
    Former-commit-id: b914d81a608c5a9bcfc52006760bb2ec7f202c88

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29c955f6dfc33cc5c1af9adb08096d6745acbf6df272a423e6f8e9fd09aad19a
+size 3494
---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/AuthController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e36013f89392a71a2f8c882ecefb0347942878a98ec4d49f89d615c1d382407e
+size 2176
