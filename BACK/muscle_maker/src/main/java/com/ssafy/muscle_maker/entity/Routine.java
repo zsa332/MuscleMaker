f85991f79d19148c3d8b87012a06e6dc55e200bc
youngseo <tim9603@naver.com>
2024-01-19 13:29:42 +0900

    Created Routine Entity
    Former-commit-id: 7991ae5b3e86c1e8b6b6bc2a689fca307f09fef8

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Routine.java
@@ -0,0 +1,25 @@
+package com.ssafy.muscle_maker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Routine")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Routine extends BaseTime {
+
+    @Id
+    @Column(name = "routine_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long routine_id;
+
+    private String title;
+
+
+}
