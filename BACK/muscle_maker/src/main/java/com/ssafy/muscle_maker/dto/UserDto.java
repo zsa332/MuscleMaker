7b4b4d441b69b805a0e4fcf629ded5828190b647
unknown <thdus981005@naver.com>
2024-01-19 16:36:09 +0900

    update login
    Former-commit-id: 8c78dd2b56c7a2e3ce7da01d24e42498011f7e1d

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/UserDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e280984b21f1d3358b8440bbeb53a11304f1ec7729cd9c9e9c27ef9a5b476848
-size 1535
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/AuthorityDto.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/AuthorityDto.java
@@ -1,4 +1,4 @@
-package com.ssafy.muscle_maker.dto;
+package com.ssafy.muscle_maker.dto.users;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
