8247a6f69bf76322c69907502b2f55532f1fdd76
unknown <thdus981005@naver.com>
2024-01-19 16:58:05 +0900

    complete user entity, club entity
    Former-commit-id: 7b8a8e43e80d4e0529c41649d8f650d30394e9cd

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Club.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Club.java
@@ -1,20 +1,3 @@
-package com.ssafy.muscle_maker.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Club {
-    @Id
-    @Column(name = "club_id")
-    private long clubId;
-
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:13ea5ec450fd93f767516c7765bbd1c55ad2b4c9fc4cff240beee05bd4c839d2
+size 755
