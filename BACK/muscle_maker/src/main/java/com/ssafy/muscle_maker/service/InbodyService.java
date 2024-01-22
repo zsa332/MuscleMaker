0d8e86b92e4472843f5a64dda3eadfddce9acf21
youngseo <tim9603@naver.com>
2024-01-22 17:38:26 +0900

    get inbody list by userId
    Former-commit-id: b4b6a549315a97b00fa21190efe11e3de05898d7

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/InbodyService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/InbodyService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9315a19b9756c6531809cd4702045e0a19812faad8c11e7ea9fff28c693218aa
-size 1612
+oid sha256:fdc825cc95dab3fadd37bd1ddccff672c0cd9c434c6be52dd17598089609767a
+size 2177
