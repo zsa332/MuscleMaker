26ee99d5d4e1ee6ee16849c43b25331fcc2b9374
youngseo <tim9603@naver.com>
2024-01-22 10:04:56 +0900

    changed _id to Id
    Former-commit-id: 19846470fdebc43a41c722ccf2c0b9b2c163bf7b

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/BaseTime.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/BaseTime.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61fbac2050d50426cc6f7deaab9071e31a0e0ffbb0bd623e36ceb153525ed9c2
-size 695
+oid sha256:e57f6bc6bb74c688f10f15cedf84af725c4bd6305770c207d77eb88229c87eb0
+size 619
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Routine.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Routine.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c9f748317fa14e5e8e756eefc1f5aebcf741bc1648eb831f4a151075bc28aae
-size 702
+oid sha256:86f753421e89797d6a2b0137472a389fc861455642774fefa70894f734146b17
+size 677
