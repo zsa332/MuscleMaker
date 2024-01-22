1ed1fa1ec2c76ae054d05643eb65d00927594900
youngseo <tim9603@naver.com>
2024-01-22 15:27:09 +0900

    post inbody
    Former-commit-id: 0ace7354fe7cd270c2d0079dcec5a91fb0230791

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/InbodyController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab910f12c6fdf249d17fcf81b0f0e08b398d934203cc6b2c33b0bce50c8ee52c
+size 996
---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/InbodyService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a308e3f99a2e0f392f29b7539dfca67d01cfb062394f056e9833634d4b8e3a8
+size 1349
