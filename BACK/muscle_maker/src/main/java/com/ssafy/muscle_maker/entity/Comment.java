27c390610d971a0802597442de57b923a7ea38ea
서성원 <zsa332@gmail.com>
2024-01-23 15:12:16 +0900

    Refactor: Test하기 위해 @ToString 추가 및 toString 제외 추가
    Former-commit-id: 91d12a357285094c0f7c64e5ddee1be3a37336a5

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Comment.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d30be11926050d384baae3791dff25e8622a8ffd904320fe29af94a6ff4c5e88
-size 532
+oid sha256:ab6ecb4417f27416d61e0ef2fdc776ee76e5578018df6416e7d1f8af1fb4520d
+size 612
