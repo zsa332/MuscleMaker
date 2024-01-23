500b7e4dd4b618f74bb163dc7fc0686407ba1581
이종재 <ljj452@naver.com>
2024-01-24 01:04:22 +0900

    Feed 테이블 오류 발생 있는 부분 정정 , IndiviualGoal API 완성 및 예외처리 완료
    Former-commit-id: f0c21663c3372feeef86ad0584de0a8c060e3806

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/IndiviualGoalController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/IndiviualGoalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:285d797e92c3bca0750482c4c47b467d3407e7cbe67d8beaf77b6f9e1b629d6a
-size 1473
+oid sha256:77de150fc0ee4f8a56d73191e470b3fcd2533e56e97481aeb072db2da9df54df
+size 3141
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Feed.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Feed.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e00cc4b36a259d43a6170d44322d9863f2520ee582314d38909d6a60e8d7ed08
-size 1017
+oid sha256:1ce76fcec7c1a3f7ad96a2738cf0005b265b75da0521459de9aecb7ff1657345
+size 998
