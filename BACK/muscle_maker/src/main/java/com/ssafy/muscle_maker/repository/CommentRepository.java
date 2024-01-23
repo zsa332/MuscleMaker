33423971b5fd8edce09cbccbc4d492930360c1be
서성원 <zsa332@gmail.com>
2024-01-23 15:13:13 +0900

    Feat: comment repository 구현
    Former-commit-id: 382685002434cca272b1ee2b3001442dc2b0c7ba

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.muscle_maker.repository;
+
+import com.ssafy.muscle_maker.entity.Comment;
+import com.ssafy.muscle_maker.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Comment findByCommentId(Long commentId);
+    List<Comment> findByFeedAndFlagFalse(Feed feed);
+}
