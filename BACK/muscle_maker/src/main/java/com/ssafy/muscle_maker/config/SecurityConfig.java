d0da82cdf17cb73bcf4f5bcc644bab05a0356fe5
youngseo <tim9603@naver.com>
2024-01-23 10:51:14 +0900

    inbody done
    Former-commit-id: d9c55cfc4734e8231db90d45da5d84fd3cced1de

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/SecurityConfig.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f02c401ac3f20f0fe668a5d5225533ef724d7165bf4ac3032044abf4a17a4377
-size 3518
+oid sha256:d026a75dfa85d01fc2d335d94b1bfd3c634372b0f7bc17d78de4b13dc994ad8a
+size 3521
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/InbodyController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/InbodyController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c70a5c58a881d11f6dccf661250b1a190994565172fdbf29e818a3d61fccb842
-size 1429
+oid sha256:160b9f2b0e210e9ff7810337058b44a40864a2859fec0fe1ae1063170ce072df
+size 1843
