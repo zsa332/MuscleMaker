f805f520cce0c3379fe3ae7e8ff611836d95f4ac
서성원 <zsa332@gmail.com>
2024-01-23 16:13:30 +0900

    Refactor: CommentDto 생성일자 수정일자 추가
    Former-commit-id: 16c3a9a58db34d4c96739e5d6891e07942dccf75

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/feeds/comment/CommentDto.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/feeds/comment/CommentDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0566d25c9f9892c4d5566110525a5e9164404602444aecee35d0f02e7eb3ccdf
-size 1148
+oid sha256:1a97d28cd4e62191336c7c5d39ed0e58a29d5f2b25fddca568ee5d56016fc51e
+size 1407
