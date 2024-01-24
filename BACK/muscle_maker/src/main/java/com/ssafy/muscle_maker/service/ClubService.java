57b913bd26dbe323ba1b90aa897748993e348e92
unknown <thdus981005@naver.com>
2024-01-24 15:54:29 +0900

    create achieveTarget
    Former-commit-id: 7477784699f737244da9548c98e0004b37e8cb05

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/ClubService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/ClubService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2161d942e306f2b4fb43bb03f24a05d73689bfd4f30a38c6f2effa193015262
-size 5787
+oid sha256:046475336112aa301ea913a1e7a8cbf100c0b301b97ae5020f1b29934d5b4b7c
+size 6201
