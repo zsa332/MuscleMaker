0a7c612c5ad271c32660932232c4599ddc21781e
unknown <thdus981005@naver.com>
2024-01-24 09:17:11 +0900

    code update
    Former-commit-id: 3f1300758a1b2ad0c8bed3e5571a4484e9756942

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/ClubController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/ClubController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b75587a3926846f1b0000cb14690505ef96ee5c42a7ce1383d91f8d4a4be2001
-size 3768
+oid sha256:76746a4d454e3c1bfe23db45262dd8bea86bc8b4a46ec341dbf2bd29992973c7
+size 1976
