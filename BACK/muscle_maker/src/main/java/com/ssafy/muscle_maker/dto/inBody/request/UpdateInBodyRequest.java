fe094e3a5e17836b2ef8edcea72083c1616a4d41
좌소연 <thdus981005@naver.com>
2024-01-25 13:14:01 +0900

    create follow table
    Former-commit-id: c5bc44a3a1ba822aa8754ca9565e601334094ce9

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/inBody/request/UpdateInBodyRequest.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/inBody/request/UpdateInBodyRequest.java
@@ -5,7 +5,6 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 @Builder
 @Setter
 public class UpdateInBodyRequest {
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/InBody.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/InBody.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:599c301bbef991199f60d6a0578246f5a8a70fda7d177a63743c83d0936fd6a4
-size 820
+oid sha256:c357c277b2314031ffcc9e3e1e347e6c5864f234acf2b33b9709ba6c9a24c75c
+size 608
