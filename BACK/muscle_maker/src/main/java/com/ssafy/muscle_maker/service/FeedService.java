ef4713579fca93796b65298257f076816d1507d6
ljj452 <ljj452@naver.com>
2024-01-26 17:58:19 +0900

    커밋
    Former-commit-id: fe60093df4a23af493378c623072d3b2d3c4fc0c

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a19ef76b241f00089b79d6da6962292b7a459e97c169f7b698f3acbf52c3510
-size 4208
+oid sha256:7ab111c681230a90e8ba1ca8e02aea8c77d335e67ee2c047fc9009bdd5a6a023
+size 4399
