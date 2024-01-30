613cad9b30b2f634af6a28203767d746d5a375ce
chan ui song <dforce103@gmail.com>
2024-01-30 09:53:54 +0900

    feat: 프론트 업데이트
    Former-commit-id: 3be1eb6061a4a678c412c7befafa989fadc79634

--- FRONT/muscle_maker/src/app/(afterLogin)/club/_component/_chart/CompletionPercentCircleGraph.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/club/_component/_chart/CompletionPercentCircleGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9037ed41a5ef49b8865aefb2d9e4c7df85385b5a45129de138646aac95666d99
-size 2589
+oid sha256:e92717e5b21edafc930ec2ab953e11aa47ac2da6e69a8e83b316ad826f81e570
+size 2587
--- FRONT/muscle_maker/src/app/(afterLogin)/club/_component/member.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/club/_component/member.tsx
@@ -1,3 +1,3 @@
-export default function MemberPage() {
-  return <div> 맴버페이지</div>
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e40423c947dffb4b2761557b52693fb720fd4c8d5ff6a5d439a54174486a2793
+size 989
