d0668077e797b30701fa691879aff7bd3de8ee6d
jgh <36934wkd@naver.com>
2024-01-31 17:14:34 +0900

    feat: 리포트 마무리, 목표수정페이지 완료, 클럽관리 완료
    Former-commit-id: 9b78853d1f436ccd717fdca73bc7459722a565e4

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/MyNavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/MyNavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf586ebd76705c42dd659fa2be9a34c9407aa6b43b01d01a0aba4e69542ed488
-size 3387
+oid sha256:3c6572bdd8876cfffd443bb126e52c6c029483499ca5d03ecd3415cf46883e73
+size 3445
--- FRONT/muscle_maker/src/app/mypage/purpose/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/purpose/page.tsx
@@ -1,3 +1,3 @@
-export default function MyFeed() {
-  return <div>목표설정페이지입니다.</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0497b941bf45f35fec9c0d4a3da4389957aa31dfb4a39344b198c7dcdfc9fec2
+size 2202
