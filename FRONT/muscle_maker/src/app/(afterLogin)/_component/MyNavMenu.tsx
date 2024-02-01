15877f08c988122b71babd3ccf632c00690d4182
jgh <36934wkd@naver.com>
2024-02-01 14:24:42 +0900

    feat: 클럽생성페이지 완성
    Former-commit-id: 62a2f87be7a9b5efb1d1910947d3705094bed8ce

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/MyNavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/MyNavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c6572bdd8876cfffd443bb126e52c6c029483499ca5d03ecd3415cf46883e73
-size 3445
+oid sha256:c1ca7bfb8be5d510e6531e8f5b3bfd806d5b6d9981fcec5b7bddbd384ec9d9b2
+size 2380
