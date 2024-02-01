5590e8a147e6facdb0f1996631d462949466721d
좌소연 <thdus981005@naver.com>
2024-02-01 17:47:05 +0900

    create initialSetupModal
    Former-commit-id: a64f422fa74637f6d1b22f1e3fc4958428b7c023

--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/LoginModal.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f20619778c231d6fde59e6bf8f358b2704218c4f62519fdd8eadcb6c8bb45232
-size 3344
+oid sha256:124b75f7d68e9e7e3fd5a2ac41cf9bc2b8c2d4c2d7c0ebafcee6b40585427c9c
+size 3260
--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/SignupModal.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a05e65a2359ca5bb93a893cf791a6caef42bc377ca5eb5220f85202c9a2c9612
-size 4835
+oid sha256:35bef54f6eb700d8496fd2cdc240611dc1614220f340448a78143cfe80534549
+size 4971
