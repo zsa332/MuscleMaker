d523890982ab43413f1adf00d1d90bd61b467af0
chan ui song <dforce103@gmail.com>
2024-02-01 17:59:44 +0900

    feat: loadingcomponent생성
    Former-commit-id: 9c14635027213fd111b759e433907501a561c24d

--- FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostRecommend.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ebdb99cb4e25bd5420b2404152ce0f9c2f4f203a2b4fab80640e57c06ab04b0
-size 1019
+oid sha256:38e37e0b3b72e16c5b83e0ad5584f8ee4087e5ce84837a43aff01cb0f88ccdea
+size 1030
