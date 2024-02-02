7a66a1048c4e5a50d0640cecb7d628f5c845e61c
jgh <36934wkd@naver.com>
2024-02-02 17:13:11 +0900

    feat: 게시글 상세페이지-모달버전
    Former-commit-id: 6b30442835fbc8a8c8ee1121d312c9b7233c4a69

--- FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Tab.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Tab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e5787f160ea07dc1202bac6351b7b8aaf55b6780063b4a31ea885a0a21de9ae
-size 1084
+oid sha256:999e0b2956f97b010b940241c57fe008ccbd6d6828cc402c4dc56caef2dbd969
+size 1189
--- FRONT/muscle_maker/src/app/mypage/compose/upload/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/compose/upload/page.tsx
@@ -1,9 +1,3 @@
-import Myfeed from "@/app/mypage/myfeed/page";
-
-export default function Page() {
-  return (
-    <>
-      <Myfeed />
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c527ec64f16f6a369d20df42956725b48c4671d43763b6fdac9fea8ef493d024
+size 5010
