31c1d9a378a83eef8219640679e342f20c9e33f3
서성원 <zsa332@gmail.com>
2024-02-02 14:57:41 +0900

    Update BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/CorsConfig.java
    Former-commit-id: c04fb2cf3b2f19541ed973fc632c87fc04ccb383

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/CorsConfig.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94a75ff8b29338b420602387b0919dd5fdbe93c672a73d03aabbc2fb4d13aa21
-size 831
+oid sha256:47cd4cc2b818b1b0301b5d5a31a118873ff8c2e7be1b3d08a9cb113a37dacc26
+size 833
