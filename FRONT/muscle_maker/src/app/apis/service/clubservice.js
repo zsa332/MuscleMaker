911f5d4047c66b5e3040692666b89684e2867142
이종재 <ljj452@naver.com>
2024-02-04 22:08:36 +0900

    api error debug, component error fix
    Former-commit-id: 3434157b68e1d76e906b9b84da2a47f933a97929

--- FRONT/muscle_maker/src/app/apis/service/clubservice.js
+++ FRONT/muscle_maker/src/app/apis/service/clubservice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20ef1fc1d07ce68a2cda188a8dcae53a4568b26601a17a815a37b3f5726f1703
-size 1557
+oid sha256:fbf413e61f1ffa9f2bbb928fec1a26899b99d3d195baec97ca994c07c3d73c75
+size 1817
