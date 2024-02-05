ef5c472520d08618be61a0fded16eee222f56059
서성원 <zsa332@gmail.com>
2024-02-05 16:15:34 +0900

    Fix : OAuth 관련 문제 수정
    Former-commit-id: 09e63d1fd45406106fff54c4d0e22439784d58a9

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/UserController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76b218961cbe6d0cdc02497005c2b095bd7fcbabfcd51a7d12d6bed21243e9c5
-size 13389
+oid sha256:f03f3ca7a932ddc936c342cc300e6d30360a975997fcfd5ea99f0abc7d06d2e7
+size 13398
