96f4883773ed87d23ba3af83dbdf32fc4843717b
이종재 <ljj452@naver.com>
2024-02-05 14:37:22 +0900

    로그인 할 때 userId도 같이 반환
    Former-commit-id: 0f7605e2e444d6ed0be509a5bd3470c48cb221b1

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/UserController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df2306859a52fa6cdcf6058f9f9932f99c30c0710ac623ad83782c294e7857c2
-size 13219
+oid sha256:76b218961cbe6d0cdc02497005c2b095bd7fcbabfcd51a7d12d6bed21243e9c5
+size 13389
