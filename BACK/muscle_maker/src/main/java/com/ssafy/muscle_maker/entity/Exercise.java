a3425891730325b850aacfd561224ad4ec36d737
youngseo <tim9603@naver.com>
2024-02-05 16:24:50 +0900

    Refactoring Basket, MyRoutine
    Former-commit-id: f939e2caa3b4df91ddad36da9963edce593ad133

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Exercise.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Exercise.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57dfb943ed83abff20509bbe612068f49b7ef479cdcf2e3da92ae3b494c98b31
-size 1652
+oid sha256:614e555b67be9f90ae9bac6f6e8ead45b27f4c66f8992cb643068ff304e27b3f
+size 1723
