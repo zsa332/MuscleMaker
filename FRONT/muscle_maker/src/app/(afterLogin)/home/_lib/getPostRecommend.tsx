d3858a29cef6cdf40ecd2f5a3bca0c5af13635de
이종재 <ljj452@naver.com>
2024-02-06 21:58:15 +0900

    feat : follow feeds, rec feeds function
    Former-commit-id: d16480d4b9d36ae8599d5c36ecbcc3cf08c1c4d3

--- FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostRecommend.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4fe9c92453b51ebb94957fe9f87d9a027cafd9ce0e31797380af43d3acd0922
-size 2454
+oid sha256:b5e734db77eae1bff3283619c6ac14e42dc2353f401f6c2a66dd0382822056e9
+size 3224
