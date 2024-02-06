20e54064e9a8e78af05f7ce795bba85c48529878
ljj452 <ljj452@naver.com>
2024-02-06 18:08:08 +0900

    검색
    Former-commit-id: e41441e25ffcaa27710691c39aabf222296679a6

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/SearchForm.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/SearchForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e382844c6a93f131ff29eb30e31f28293537b8f4d738a05a878432850c039fc
-size 1640
+oid sha256:d6fb2c5e7c9d5f942ffbbad6670074381878a398e30d837328a38ed691a6eab3
+size 1909
--- FRONT/muscle_maker/src/app/apis/service/userservice.js
+++ FRONT/muscle_maker/src/app/apis/service/userservice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aabe3e729a630ab14f3f60924320e99ccce6b1501d57f6d6761ce2163d42730e
-size 5812
+oid sha256:79f12e1a8e62d02735a74dbdcc3f35b8bd7a0caf94878d4dff1928ee39d55d1e
+size 6045
