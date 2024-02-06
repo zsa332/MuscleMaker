502375e46c0db99bd5ac5b49086b83a1d96bef9d
이종재 <ljj452@naver.com>
2024-02-06 22:26:59 +0900

    글쓰기뒤로가기 오류 해결
    Former-commit-id: ae49f177b6d78c3a28a24fdbae554cdb04ca8171

--- FRONT/muscle_maker/src/app/(afterLogin)/@modal/(.)compose/upload/page.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/@modal/(.)compose/upload/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:091305a9adeb5f64d72438084eb7efcc1f20dcd255c61ad54e1f9ccff9251b28
-size 5130
+oid sha256:6f9fb38470f476b63d93221391ef1632a2f590bc718e3ea3ad05030c5e33babc
+size 5103
