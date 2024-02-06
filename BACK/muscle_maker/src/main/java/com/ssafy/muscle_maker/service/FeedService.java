35113eb6f9a7ab5350de34595a96670a0a69f731
이종재 <ljj452@naver.com>
2024-02-06 20:43:06 +0900

    feedDto : user.nickname 추가
    Former-commit-id: 86fbeee29724c90f9141664d6b0c02184da6143a

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:841afc39333bdbdf7491c1a97804e719cd97cdb2ab731f71de9bc55177ef6feb
-size 10387
+oid sha256:51285199445de810ddcc3aeb4a28f9c4bda2d42b086266925d6dac6353113537
+size 10835
