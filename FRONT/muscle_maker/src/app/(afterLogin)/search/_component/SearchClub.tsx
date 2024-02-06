478d0d5f5650cffc6cbc8d487568d9e1ff4a3cdf
이종재 <ljj452@naver.com>
2024-02-06 20:38:03 +0900

    사용자, 클럽, 테그 검색 및 디자인 개선
    Former-commit-id: 52c522bf72c7163308cda1d8ce889059b606bc96

--- FRONT/muscle_maker/src/app/(afterLogin)/search/_component/SearchClub.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/search/_component/SearchClub.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99fa8240a6e98213e3e0765fbe03cbcb4daa3ef78790b61d817fac747a6a58c4
-size 1606
+oid sha256:5456dfc5426a79839942f3ef501f23e27de9eeef81670f053eca6dad2d62fc59
+size 1938
--- FRONT/muscle_maker/src/app/apis/service/clubservice.js
+++ FRONT/muscle_maker/src/app/apis/service/clubservice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9037f7103c04d1726af7446285a32e3b1ad4d71d16010c9e17f11ec0c8c74e1d
-size 2097
+oid sha256:fc227641d1347da1a01eb5b2186e7955a3d6063af61170f6e2f39b40dd6f3784
+size 2098
