7659117c880bbcbba4e19769efafe8d023bd5fcd
ljj452 <ljj452@naver.com>
2024-02-06 16:55:16 +0900

    feat : user,club 검색 결과  출력 기능 구현
    Former-commit-id: 5d5f305eb04ebffc22dfab15bf3bfd37bd47d3cf

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85e61926d3bb0ca31fd4b60c1324353abb69e9f8a5c722ea18218332f71f25e0
-size 5836
+oid sha256:aca1160817825cb6ba9597fbb9417d66972e9e703898b0fe23115a44e3a6625a
+size 6368
