8d68cb81b12c5f07a47c9fabe017272e0b71b2c4
서성원 <zsa332@gmail.com>
2024-02-08 03:24:14 +0900

    Feat: 알림이 필요한 로직들 알림을 보내는 로직 추가
    Former-commit-id: c9d78c19143efd6493d86d321d1a97c5e26f1be2

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6080699ff183cb96fc00315ef69dd64b5a010cefd54ecd848859b9dc07f88a5c
-size 2217
+oid sha256:a71b8d46bdc1fcbe04718f73a3566df536c7d86b5e0dbcb4abe27e95c9245e53
+size 2514
