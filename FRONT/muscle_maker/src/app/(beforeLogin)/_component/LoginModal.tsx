9a9e47ad9ca61961e7bfe905e780f511cd96f355
좌소연 <thdus981005@naver.com>
2024-02-07 14:09:56 +0900

    resolve conflict
    Former-commit-id: 5e6acad941e6fa713907d53e0ff6039bdaa78a6f

--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/LoginModal.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4edf0ae8e05ed3ce0c172226e635e521cb6e45286a10b96288e2ff23cdd77918
-size 5594
+oid sha256:e2f7804ccc77e2f9951719f44e787b44f4360cf354363050a1571aa5ce8e3a49
+size 5248
