b30b88bfe43078d759ad9efbec3cd3023e011b61
좌소연 <thdus981005@naver.com>
2024-02-07 18:25:41 +0900

    create intial target set up api connection
    Former-commit-id: 01aa9c45bb54845e689d95d982f0c13e4184bf47

--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/InitialSetup.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/InitialSetup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1210c792116d56336ed401dafea7ecf95e9cab50de6ac12b7b107cc72b79222
-size 7866
+oid sha256:f14700394180cc42676ccb2ad920066ec411312d9b1d4fd793ccef907d757d16
+size 8288
