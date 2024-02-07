c921dcc8fcc35cc82b76ab13bd24bcae50800b49
서성원 <zsa332@gmail.com>
2024-02-08 03:24:43 +0900

    fix: connetion pool 반환 에러 해결
    Former-commit-id: f9d65c6f9af9b800b07c3de085f5cdee60edb429

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/SseEmitterService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/SseEmitterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b8710885b6e9d8ac632edccb0fdc286ee3413cbbf5670bf7593ffe3a8455bd
-size 3561
+oid sha256:18edf1b5785b9a43bed06fddd4ba478f217992eb1c60bc8924097a21ed91e036
+size 3589
