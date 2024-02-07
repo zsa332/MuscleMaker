7331b3cf501b40207894d484c1611066b934ce29
서성원 <zsa332@gmail.com>
2024-02-07 14:42:06 +0900

    develop/be merge
    Former-commit-id: b6e5c982610c8aa3bb264859f8aca1c224371b8d

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Exercise.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Exercise.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b1fb59e8887d1280db6572b2de20f8b8dfd2cbf347472241536093fdcc515d9
-size 1061
+oid sha256:614e555b67be9f90ae9bac6f6e8ead45b27f4c66f8992cb643068ff304e27b3f
+size 1723
