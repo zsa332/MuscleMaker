7905ed1dd78e39b66783d14d6bad9ce75b52079d
서성원 <zsa332@gmail.com>
2024-02-08 03:24:43 +0900

    fix: connetion pool 반환 에러 해결
    Former-commit-id: e8a9ce55578fd202d5e3d7eac46155da4c2e6b5e

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/NotificationController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/NotificationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ccf2ec6eb97d031d8e61826d2610bfbe0b6c24edaf96807f1bf22752c9b2661
-size 1452
+oid sha256:e64288dd6071f80b4f0097c1c5d5cccd4a948d8382d0c3ec3ac4b3a9b65e1585
+size 1770
