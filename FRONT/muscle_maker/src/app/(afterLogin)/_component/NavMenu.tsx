e1ed412fc27c1d1e4d44d4fe2103f196335838e5
좌소연 <thdus981005@naver.com>
2024-02-07 17:56:27 +0900

    update notification navMenu bar
    Former-commit-id: 38af29f5da052f3b533b5d594644139d2cc82a7a

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd99fcdbc5a7fd8d045ebbec69c32e95ed541bb435f0458c315bd751d714562f
-size 3253
+oid sha256:8a6b1b4de9e448e53ed472ecb3ca777f168a79778c12381d36bb78703911c305
+size 3402
