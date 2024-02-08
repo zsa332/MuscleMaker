68a821647ab1767a059e486343677e7187c8e39d
jgh <36934wkd@naver.com>
2024-02-08 13:57:30 +0900

    모달 오류때문에 이름변경
    Former-commit-id: a0619ccee7a442786f309478b5a1b15f4f905e3c

--- FRONT/muscle_maker/src/app/(afterLogin)/detail/[id]/page.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/detail/[id]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4facf790057c4ad745e522695073b8b9586ceca1c5bcaab3a33fbc4d2577f37
-size 959
+oid sha256:fe7e27f0413b9c40b09f1cde0f10e4f13c9272b1b501a53fa8517a4f09b0bec8
+size 967
