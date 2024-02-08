bbb1a2e4d6bec1bc1f62af51f948e337b6008cf3
youngseo <tim9603@naver.com>
2024-02-08 12:35:17 +0900

    Refactoring, signup, consider when image is null
    Former-commit-id: dd0f7137c1a842406ee16b5aed5db3c15e7ae71d

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/InBodyService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/InBodyService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30696f441ceae55688c4f790286f9d6397c5f7a4ccfced1bb51eea985ef3a5dd
-size 3849
+oid sha256:673abeae63c68b75cdf6d9acf5d61a6982be4429d6cf9026e937667cb8b5f33e
+size 3850
