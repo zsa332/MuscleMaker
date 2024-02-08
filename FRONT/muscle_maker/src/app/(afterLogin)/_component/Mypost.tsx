856e82fff1ed81ab787e2afe470ddf0b0529b840
jgh <36934wkd@naver.com>
2024-02-08 12:46:29 +0900

    feat: 마이페이지 - 마이피드 연결 완료, 유저페이지 링크 추가 완료
    Former-commit-id: 217426dbc1f301fd59f853102a02cb72c87e9d35

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/Mypost.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/Mypost.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fa6a897808af355d8e083c46f921aa0367e3f5f04a4b8785b69dbde220d7916
-size 2082
+oid sha256:1b6e51ed6386602b9c21460cbba66602dd3ea49039a70a6106220eeb8df35b4c
+size 2906
