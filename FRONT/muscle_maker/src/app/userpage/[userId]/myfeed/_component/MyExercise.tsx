aaa62cab877ec3b18bcff4beeafe1a4a9a3b4587
jgh <36934wkd@naver.com>
2024-02-08 12:49:05 +0900

    feat: 유저 페이지 - 마이피드 기능 완료
    Former-commit-id: 055d7e5caefc793a7084a914a12352f7a7ed45e0

--- FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/_component/MyExercise.tsx
+++ FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/_component/MyExercise.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a029afa6684edc35218fb1bbd8b5b451ff611bea37ebcbe001a5ff442216e477
-size 2443
+oid sha256:728e0b4dfa8b65278771b733ec1d6236e54433988530b7ebf558dd90c7c9bde9
+size 2571
