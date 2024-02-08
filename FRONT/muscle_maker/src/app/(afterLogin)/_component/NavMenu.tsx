59a09349b6f6a49bccf5fb291ace6f98526d096d
jgh <36934wkd@naver.com>
2024-02-08 13:12:38 +0900

    fix : 빌드오류 네브바 스타일 경로 수정
    Former-commit-id: f4096ab3ad41bd1eb7df5340565c1aee7eb42fc0

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a6b1b4de9e448e53ed472ecb3ca777f168a79778c12381d36bb78703911c305
-size 3402
+oid sha256:ab72b524ba82c0f691e0c403695ba3084c543b38b1a905e4dc7fa78f3c8aeb85
+size 3502
