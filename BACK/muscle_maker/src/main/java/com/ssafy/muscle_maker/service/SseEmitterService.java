3fd452d3f7c1805dd95b1a119f7332b2be34210c
서성원 <zsa332@gmail.com>
2024-02-08 11:06:30 +0900

    Refactor: sseEmitter timeout 제거
    Former-commit-id: ec290072973386515a2bbf2f6e5ab05c27ea9a4d

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/SseEmitterService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/SseEmitterService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31af1745fe7ee61ae0bc8d2972286c9857466776724bbae3f09248944c1c06f1
-size 3540
+oid sha256:efe446ab3c16e99b3d6e24d132fae2bde7edb94bc9a45d6318266f520819d2f9
+size 3525
