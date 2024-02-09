f0e9903e59619277a6ba4a6459f516e6037c1260
이종재 <ljj452@naver.com>
2024-02-10 00:15:49 +0900

    profile cnt update, signup page error sovle
    Former-commit-id: 5afda5966dc5776c0f8221518cb7f1eae4c106a7

--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/InitialSetup.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/InitialSetup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42b40908ac3f9b012b16a8413d440c2bd7150017081300cbc82a409beca15f07
-size 9800
+oid sha256:788f375cdc010dab90e5ea0d4305a9e079da3fae61e6aec43cfdca6e97e5a4d8
+size 9830
