b5becd2710e7759371d138ddb77cd1b244117970
좌소연 <thdus981005@naver.com>
2024-02-10 22:17:24 +0900

    create follow modal connect with follow button and follow api connection'
    Former-commit-id: a53fc6bce3b9b5320dacc4025af27a6778c4fe0d

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/TopProfile.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/TopProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71e7c4c0eec9b276e554fa85c16d156f3ea35fff29f6d1f7d31a2b58454c4e09
-size 2696
+oid sha256:4b760a8e043e1e0f8911cf778195ae2a149be5367e726ef24f870595fcdc8453
+size 3789
