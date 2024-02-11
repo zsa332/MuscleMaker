54251867d8635bc98c252b19b72c95da5dbb720a
이종재 <ljj452@naver.com>
2024-02-11 23:26:37 +0900

    api error solve
    Former-commit-id: 83e8c0ba54f13cbd26d6bc562ed710f21026e648

--- FRONT/muscle_maker/src/app/apis/service/feedservice.js
+++ FRONT/muscle_maker/src/app/apis/service/feedservice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09256df142634966ff7e2d393f70494018e83a87c8bda0cfcf6cb6ca529aa17f
+oid sha256:213e1231b47bac4c4e0fc89aca1b8489df01ff731736f63049a10b6fccfd9de9
 size 6343
