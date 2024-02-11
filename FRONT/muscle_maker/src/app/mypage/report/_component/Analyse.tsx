e5664664271da0e7b32e4515e572f419e0b5035b
hello1334 <36934wkd@naver.com>
2024-02-11 15:00:24 +0900

    feat: 인바디 분석표 분석부분 완료
    Former-commit-id: 11a7e5f589c38392b824150745b8b49fd73d2dd1

--- FRONT/muscle_maker/src/app/mypage/report/_component/Analyse.tsx
+++ FRONT/muscle_maker/src/app/mypage/report/_component/Analyse.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2340ffa23418e661d01bb8b5638d0f52e5cd37fbc339a9e92a19b9d5bb7cab89
-size 4911
+oid sha256:bb731f43ae4d8d2db2349d373d564e2d1e91546721b5b11c1343eb7c2990cf13
+size 8592
