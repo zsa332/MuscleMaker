c7c8f2534caa2d1f72cc1290166af9101e031cc4
hello1334 <36934wkd@naver.com>
2024-02-12 23:09:39 +0900

    fix: 네브바 스타일 변절대경로로 변경
    Former-commit-id: 003098536867322b990b73eb08f198021e5f89d7

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93032e470dc631dbed19b109c35d702047bcfdfd6d7c98cafa12022b085595e5
-size 3825
+oid sha256:210d61b97401eabd00e61b0e53898b70142b314bfc31535d0c8f85ad92744a5f
+size 3938
