a52287dda1660e8c13f42d781623c5ed91391883
이종재 <ljj452@naver.com>
2024-02-13 07:00:22 +0900

    클럽 생성, 다른 사람 프로필페이지 요청 받아오기
    Former-commit-id: 2b42372dfef3b6c15860db137e728b25a276bc74

--- FRONT/muscle_maker/src/app/mypage/@modal/(.)compose/upload/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/@modal/(.)compose/upload/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c8c4d9085894b3a2f46e8fb345b22aa5fb7d6f3f4fb9df41552d357a20e2ef7
-size 5017
+oid sha256:a016e03cf851e012c97c506424c737a49650e56aabf059e45ca457c81a690e15
+size 7355
