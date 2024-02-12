0774b098d45b1997d0d7af1003f5d814b2ae976c
hello1334 <36934wkd@naver.com>
2024-02-12 20:46:39 +0900

    fix: 마이페이지 - 마이피드 빌드에러 해결
    Former-commit-id: 8f77e453c21e649a7d946a42737ef4214b418e45

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/TopProfile.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/TopProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01b182982262749467de10ddb44fd31a48a3365f5b7c9bbc5398e6835737fc56
-size 3828
+oid sha256:c2446ebec86f42e46966d17e2a64ec671cb137c54e3443e3a5e1fc266cbccb91
+size 4218
