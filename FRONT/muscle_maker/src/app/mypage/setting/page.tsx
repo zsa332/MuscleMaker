2f62894e682484bfb772539dcbf12f93df081b53
Chan <chan@Chanui-MacBookAir.local>
2024-02-13 02:09:28 +0900

    feat: 세팅 컴포넌트 생성
    Former-commit-id: b48e4e3503a831875bd88ddbf6d50d9846d72f30

--- FRONT/muscle_maker/src/app/mypage/setting/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/setting/page.tsx
@@ -1,3 +1,3 @@
-export default function MyFeed() {
-  return <div>세팅페이지입니다.</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5d60ac4eb14e151cc632d151a8356fe80ef791259e9c5b48a90c1d7719fd796
+size 1112
