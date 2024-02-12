46f317e6829800dc19adcf553edbd1d2d5cb485e
hello1334 <36934wkd@naver.com>
2024-02-12 23:02:43 +0900

    feat : 유저페이지 진입 시 본인 아이디면 마이페이지로 보내기
    Former-commit-id: dd08be17a6e2c4b9c4c98aab317d19aea32e1992

---
+++ FRONT/muscle_maker/src/app/userpage/[userId]/_component/FollowButton.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4a71a9315439b0415f46ca3471315ccaa1f89db599e97a2390c4439e804abcde
+size 1050
---
+++ FRONT/muscle_maker/src/app/userpage/[userId]/_component/fackuserdata.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39aa76ade6af85d0857412a7c6111b7eb791a876b2de4261e35f93a5887df951
+size 2156
