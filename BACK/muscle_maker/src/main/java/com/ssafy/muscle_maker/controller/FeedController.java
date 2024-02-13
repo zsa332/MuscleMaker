46d94492291c74d6dc6e48fae0f12e676be1965e
youngseo <tim9603@naver.com>
2024-02-13 13:11:28 +0900

    Refactoring, change get recommendation club
    Former-commit-id: 0e544777d7606efad13dd5f63db7fe9c0d3701f8

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/FeedController.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/controller/FeedController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f15c0bb2533bdb72258673a0e974a6d636c95cbfbd02e4e746507515d71bce14
-size 7947
+oid sha256:93774bddc35146a5d0878b4f5af500b031cd25cf1e2558e62438fe7d1c70f2d8
+size 7901
