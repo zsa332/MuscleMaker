4e4b8ee09eb459a5be0aee6765801c640c55323a
jgh <36934wkd@naver.com>
2024-02-13 18:09:55 +0900

    fix : 부족한 css
    Former-commit-id: 8b6828dff9ed761d9407c2fa59a2198745849663

--- FRONT/muscle_maker/src/app/mypage/myfeed/_component/MyDiet.tsx
+++ FRONT/muscle_maker/src/app/mypage/myfeed/_component/MyDiet.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be81cdff7d8a7cfe85941d93989e2f32622d289be7af2ef24bd0a9ab3f99f192
-size 2608
+oid sha256:9b1e36763b055115185928c5bfc15c97a37111b03b4e3b472aac41af203c6060
+size 2383
