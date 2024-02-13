7e774046689a67ee82dbeded0f18de549b42901d
youngseo <tim9603@naver.com>
2024-02-13 13:11:28 +0900

    Refactoring, change get recommendation club
    Former-commit-id: e8a15e522c8189b746ea70c4b96d05678b6023b6

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/ClubRepository.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/ClubRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:944bf4220a06b2bbbd342f8441db6b3debea180e275bb883346f3a1e4bdcae21
-size 677
+oid sha256:8f3e824c2d18a6747f4c633c7c294d3acff229c444cb6490a5f52005b98f6763
+size 891
