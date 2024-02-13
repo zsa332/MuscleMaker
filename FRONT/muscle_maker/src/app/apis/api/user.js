c97bdf5aee5832883e660499da933da2b34c1263
좌소연 <thdus981005@naver.com>
2024-02-14 08:14:38 +0900

    create recommendation user api connection
    Former-commit-id: ec3c12a56eb9e873c8941fb5b7cb406883bc47bf

--- FRONT/muscle_maker/src/app/apis/api/user.js
+++ FRONT/muscle_maker/src/app/apis/api/user.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16d238f219f8659117059d18b62e9615d8042641c3f789a13e24d49d74498a1a
-size 2177
+oid sha256:6017c959e1fb987deeac122fbe9acaefd0b00da3d962dc1fb674b19659e413ad
+size 2333
