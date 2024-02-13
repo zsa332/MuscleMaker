7be5f1db60e89ce97f6fc5a23a6250c6bf56e5cb
jgh <36934wkd@naver.com>
2024-02-13 10:56:40 +0900

    fix: 로고 변경
    Former-commit-id: f16f237a5f2901c24d6f7819060d4b787a307733

--- FRONT/muscle_maker/src/app/userpage/layout.tsx
+++ FRONT/muscle_maker/src/app/userpage/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:906229764da7c0c87efb05dfad217077c7d175ec8dd3db66b4dcee732debb09d
-size 1843
+oid sha256:7293c43d44b79e7f6ec2b506a15277b79613f401dc8ea158d7c7a94a268a4cb0
+size 1895
