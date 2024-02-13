3fc1ae292c9511b6b1788176ade085c114c70a99
jgh <36934wkd@naver.com>
2024-02-13 20:31:23 +0900

    fix : 마이페이지 - 리포트 최종 완료
    Former-commit-id: 856c35a8b3b2a008146073fbf1a02dc0be0e353a

--- FRONT/muscle_maker/src/app/mypage/report/_component/Analyse.tsx
+++ FRONT/muscle_maker/src/app/mypage/report/_component/Analyse.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:160a96494cc36eb1d867f3b1bb83d28a6b1eba0fc7bb8afcda2ab7ba1d445f34
-size 13153
+oid sha256:6e045cda8dec6dbcca7ca5fc76dcf64e5dc2bafcdf36e80e91c1651956cf4928
+size 12984
--- FRONT/muscle_maker/src/app/mypage/report/_component/MuscleChart.tsx
+++ FRONT/muscle_maker/src/app/mypage/report/_component/MuscleChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86787f32cac6b5abbbaee5b99a1ea3e6b4557618063ee1a5c3f7456dc7883183
-size 3407
+oid sha256:a5f5e61129060ee36207bae4ce393680691c433caa0aafecd05608600f159bb3
+size 3525
