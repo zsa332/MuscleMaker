c3d7f2a9a81b94ba8aa393ab44a7ee9e12e68569
좌소연 <thdus981005@naver.com>
2024-02-13 13:55:29 +0900

    update initial target set up ui
    Former-commit-id: 02eff2447d9f080232404427ff801a39849e0d7f

--- FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Feed.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Feed.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a5e8b59e5d5d4b4005de97dfba2d443172616860b83b2d5f20f9d2ef963cc74
-size 2753
+oid sha256:fff86a09aa5111bee17d8aa3bf259b432372cf59a18437cb15edd55cf42a484d
+size 2762
