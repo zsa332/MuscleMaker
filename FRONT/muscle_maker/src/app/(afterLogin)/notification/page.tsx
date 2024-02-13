aca85e76173f5781c9e1ce41446e18a8ce6cdbb3
좌소연 <thdus981005@naver.com>
2024-02-13 17:51:33 +0900

    update club page, recommendation page
    Former-commit-id: ced520d9540c643ec95c909f71fb79ac4afd2965

--- FRONT/muscle_maker/src/app/(afterLogin)/notification/page.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/notification/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09c33ed00d86e43471ba156304556a07416fa5eaba3a26e2529cd77c0fd7fa52
-size 3773
+oid sha256:4a096f2047d8309053b3a8383aade8fcef16e0d46c9189ea6a6f0e29894cce4f
+size 3776
