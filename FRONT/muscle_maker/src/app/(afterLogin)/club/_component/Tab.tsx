1b7cf8310fecc223409960f23b62732efdb3b954
좌소연 <thdus981005@naver.com>
2024-02-14 21:28:42 +0900

    create main feed page
    Former-commit-id: 49b94a3b2b4962c489279b81d5710779ff8374be

--- FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Tab.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/club/_component/Tab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73010683e0b5bd63417a347dd5891698e2c5bd537205cac09e75ccde11cf71ce
-size 5488
+oid sha256:ca3ee44052a9e998195016b53cbd7e8239e1ba48474f37f4f67e5fbf15b878ff
+size 5657
