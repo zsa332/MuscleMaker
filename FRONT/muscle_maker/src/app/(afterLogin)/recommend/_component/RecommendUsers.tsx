423ebd3cc8565f241652c679c7860c7cfda24046
좌소연 <thdus981005@naver.com>
2024-02-14 13:40:30 +0900

    create club feed api connection
    Former-commit-id: de8c609e1f5de3f6a3e2d6099113fcb34308f1d2

--- FRONT/muscle_maker/src/app/(afterLogin)/recommend/_component/RecommendUsers.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/recommend/_component/RecommendUsers.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de51f2df0dfab649562af7db933872ce8fe8c8951b3d3ccf77438ba2f86c7dea
-size 5147
+oid sha256:739017ab243cad2fcd8fb626c4f09b9c7ac508d624510452b95bddd2bfa02973
+size 5148
