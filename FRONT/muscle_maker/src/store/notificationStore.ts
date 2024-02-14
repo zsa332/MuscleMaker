8b31a3036559911cdc3a80c1d07db4a2b7d29b4c
jgh <36934wkd@naver.com>
2024-02-14 11:03:42 +0900

    fix : 알림 네브바, 알림스토어 빌드 오류 수정
    Former-commit-id: e27a71a5cd7c7665579c77689e9bbcf1a06cafe3

--- FRONT/muscle_maker/src/store/notificationStore.ts
+++ FRONT/muscle_maker/src/store/notificationStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2deceb3a43f996d629e400631cef006e6a060af784d1ce9bc6951df52f7e9794
-size 1202
+oid sha256:3bd45ebcd96ebcddedbdba6621131bcf7bf8936f893fe86d5ff272b69684a121
+size 1199
