36574e28db004aa3a4ba1e6fa12433051e5ab2f4
좌소연 <thdus981005@naver.com>
2024-02-14 13:06:37 +0900

    update recommedation, notification api connection
    Former-commit-id: 46cad9287a2024e835c01e34d016ec62dc852f4b

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/FollowButton.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/FollowButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a71a9315439b0415f46ca3471315ccaa1f89db599e97a2390c4439e804abcde
-size 1050
+oid sha256:6e017718405347e24b560a9dcc22576d0f20c7e7617dab83081cdccbb5ce7f4f
+size 995
