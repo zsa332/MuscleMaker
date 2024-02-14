2e57701391948620784dcc3d6057c263de786578
ljj452 <ljj452@naver.com>
2024-02-14 10:09:20 +0900

    feed api 수정
    Former-commit-id: 4b14d2ccee7d584009bf2895f0a9a92a8951e5a0

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2318a69aa9be53f02e05cf04a8ae6ca5a2958a870ecc7fce4d8fc678b85a40
-size 14339
+oid sha256:8038a3f68033c1a4f7f8ecbb3d8542059b99266550fadc80f735ae29082a117b
+size 14337
