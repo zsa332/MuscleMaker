ac475086d0c4451e65397cb3ab8b1c6279397792
서성원 <zsa332@gmail.com>
2024-02-14 13:26:18 +0900

    Refactor: createAt 시간 지우기
    Former-commit-id: 552b913c148fdaa04bfee920f342cd141ceb681a

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/feeds/request/FeedDto.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/feeds/request/FeedDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e7b58af1c180afe4498c39819d1fea8dfa835c86794e70ce5df2528fbf68120
-size 1703
+oid sha256:d78831406c9512d508333db8d67ac88f9a60cb599f738da80793de5a6e170348
+size 1737
