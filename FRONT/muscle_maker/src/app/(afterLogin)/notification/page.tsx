a1a425bcc703b69081ba5256543e19da7deb922c
hello1334 <36934wkd@naver.com>
2024-02-15 02:07:44 +0900

    feat: 알림에서 상세 게시글로 스크롤이동
    Former-commit-id: 95036dc1172d65a694f79cf3a3e9aef4459006ad

--- FRONT/muscle_maker/src/app/(afterLogin)/notification/page.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/notification/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ef30ff9746294bb6b9f1c5da540fccd3f9894270710fb16e86b0cadade611a9
-size 6533
+oid sha256:f554d43d3839808b7b06f774ac082fb10948c8ba57aa012eb84a9bf0727c725b
+size 7198
---
+++ FRONT/muscle_maker/src/store/homeStore.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:74018bd969d2e0b206437a6cee1f95c99d7eab02fd068d83c94a6b0f011ab5a9
+size 743
