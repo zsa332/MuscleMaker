5a8b125507cad5bf29f6de28baa2828e69e29a71
hello1334 <36934wkd@naver.com>
2024-02-15 02:18:23 +0900

    fix:스크롤, 모달창 and 빌드 완료
    Former-commit-id: 32151ff4efc4deabe2a4fb69be595c005b4ee23d

--- FRONT/muscle_maker/src/app/(afterLogin)/compose/upload/page.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/compose/upload/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e093656b80c56bf8d384611925b5006bf020eb2ec9641e74419524aeab96b726
-size 4491
+oid sha256:5412595f54195567235bb0ccd990b3aed3f0287b3e70b1ece4dc7c06372be158
+size 10017
