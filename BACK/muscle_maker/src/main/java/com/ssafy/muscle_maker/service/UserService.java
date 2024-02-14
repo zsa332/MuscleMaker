b747720f6c5c5a13b1f813d3513ef86ba6857097
ljj452 <ljj452@naver.com>
2024-02-14 11:31:05 +0900

    User BaseInfo  내용 추가
    Former-commit-id: eb6c339232ef8606f2e59098876ee4ec49c2a582

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e92ebf260500858ecd85f9187dddbe4537a103a3a998d200cc506b795bfdadef
-size 10391
+oid sha256:b7e1317ccdb403e3a1cd1614960818dbdb3b5f6fbc05486a9d8d7e1bbcbc9cb1
+size 10479
