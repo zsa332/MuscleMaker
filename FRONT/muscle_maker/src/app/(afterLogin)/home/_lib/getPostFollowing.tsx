1870b49355a5b0d95bea7b36a98e72ab643c057c
좌소연 <thdus981005@naver.com>
2024-02-14 22:10:24 +0900

    create feed api connection
    Former-commit-id: 95cbebdb25e0ac948cef31f03c0730687034d7a4

--- FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostFollowing.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_lib/getPostFollowing.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4de7c3f47fbbdd349f1928ad8cdf890863bb97e2080bae9868ee73e060bbdef9
-size 12462
+oid sha256:da1a630ce6f95a6f91e2ee16ed863006e7f0e9e5e7a81d6eee8459a48d6ff11b
+size 7596
