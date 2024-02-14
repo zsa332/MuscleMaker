07e5264f77295bd4e2bd18092b7569afb32eab35
서성원 <zsa332@gmail.com>
2024-02-14 21:18:30 +0900

    Refactor: 댓글 작성자 닉네임, 이미지 url 추가
    Former-commit-id: a03ffb93c96840413cc75ec73d1917684608f662

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d50aa3144fb5597f381ad94f68b81ab5a7f51c324cd003fe398a38903f02a62
-size 3010
+oid sha256:295041a976f56cbe2c88716a006582ad10484b491efb95465b2155bee0b247dc
+size 3220
