7ebcb9e3d0d5917f3ce05a365dbb7ff219bf24fc
서성원 <zsa332@gmail.com>
2024-02-14 13:45:39 +0900

    Refactor: feed comment cnt 수정
    Former-commit-id: 997848eabef5f27b1a31a58d620866ae8afe773c

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40f80b00724a50ff8ec64a081d27fd97eeddfefb9eee6a03795e8461c0a7b113
-size 2964
+oid sha256:0d50aa3144fb5597f381ad94f68b81ab5a7f51c324cd003fe398a38903f02a62
+size 3010
