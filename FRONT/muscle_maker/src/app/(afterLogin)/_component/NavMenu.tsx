da50b81bb545c11ebe184c1e100dc6850b0435da
jgh <36934wkd@naver.com>
2024-02-14 11:03:42 +0900

    fix : 알림 네브바, 알림스토어 빌드 오류 수정
    Former-commit-id: f66fdb91d9511ff09cb24bdd3b4de5df8d0287a4

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb646fd8c37fca012743a9a5c10264b7fb279ab53424d6515a3d47a49cc0187
-size 4281
+oid sha256:ccd6c7e3815fcb426c622110ac5465bacfbd42a1cee83baab35099b6af3ea276
+size 4276
