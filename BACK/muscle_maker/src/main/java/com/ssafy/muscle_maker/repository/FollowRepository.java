9150c898f8f5697981897fe150dac9f7f31004db
이영서 <tim9603@naver.com>
2024-02-15 00:46:36 +0900

    Update FollowRepository.java change follower to following
    Former-commit-id: 34e5863aa7446a6180e4e86ebea1a98444559cd3

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/FollowRepository.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/FollowRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55a9888989845187349ad67f6a3374de9f01e8ea7c5f181cd6eece5bd0181df2
-size 1824
+oid sha256:d7220f8e7cb205ac66f6cb419a848b391aed997f046d2b1e55b29b7a14edd6c4
+size 1826
