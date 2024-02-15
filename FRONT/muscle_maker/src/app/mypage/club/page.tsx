aeee2239b4327ec0e21399b279f680375d8b3c4d
좌소연 <thdus981005@naver.com>
2024-02-16 02:07:42 +0900

    update entire page ui
    Former-commit-id: 5f80ec9e1910d619413750b11b05a66269f4a6a1

--- FRONT/muscle_maker/src/app/mypage/club/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/club/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e34bc350bfe8da83d0145b3c28f5e26e2a827ead79659c41e93b7f0d1f47aa0
-size 2122
+oid sha256:a568d2883e249112d5fe819d044cc24a28dfced98fcfb301081148b76691c3b0
+size 2167
