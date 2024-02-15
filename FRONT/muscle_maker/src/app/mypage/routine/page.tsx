781116468f162752dae03b42880b64f69baaa675
jgh <36934wkd@naver.com>
2024-02-15 09:19:32 +0900

    fix : 배경색 css
    Former-commit-id: 30da18fb8c783d029232e888352b6e9f14b2d8d9

--- FRONT/muscle_maker/src/app/mypage/routine/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/routine/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:831ff16057c80e6e6790d66b8b644e03a91c38a874ee071b1a19864ab5ccef97
-size 821
+oid sha256:7aaf28d3a9b650274fc6a9afb57cc4fce8f6b9771c6429b249e6aff445c88272
+size 738
