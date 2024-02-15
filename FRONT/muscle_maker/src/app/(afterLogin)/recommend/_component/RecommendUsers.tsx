887600dd936b2ff12a18722ddd18b5a0f2ba40a0
좌소연 <thdus981005@naver.com>
2024-02-16 01:47:59 +0900

    update my page feed UI
    Former-commit-id: 3c4d51df00d3262c51a779423ba0a7203d224822

--- FRONT/muscle_maker/src/app/(afterLogin)/recommend/_component/RecommendUsers.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/recommend/_component/RecommendUsers.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bedccdc2af10c2182d1be36363cdf660f985bfe8987521e8f35d7f1a2af9044
-size 5575
+oid sha256:719a547f81203e2a6b9a0716b6a3aba2fe0097ae951fa2da0654b527920b99a9
+size 5581
