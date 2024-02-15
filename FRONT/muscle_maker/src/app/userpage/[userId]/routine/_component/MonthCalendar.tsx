615ddef60e2f64adb973daa05c0785ef257a71b8
Chan <chan@Chanui-MacBookAir.local>
2024-02-15 11:12:08 +0900

    fix: 월별 수정중
    Former-commit-id: 7ce0224df667edbd8f1961c19e9c0d186019685b

--- FRONT/muscle_maker/src/app/userpage/[userId]/routine/_component/MonthCalendar.tsx
+++ FRONT/muscle_maker/src/app/userpage/[userId]/routine/_component/MonthCalendar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01939591f8ad907b7bfe2689f1e733f5bb9b8d39801006e411de2e929dac1295
-size 7395
+oid sha256:8bb965ff9ec54697071bb0acf10a6a1642eefffea3ee8ee36b16da4802535cb7
+size 7495
