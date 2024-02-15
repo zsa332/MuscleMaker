d0f67e608b36ef60e0133f1dd215c1e5066716a2
youngseo <tim9603@naver.com>
2024-02-16 01:42:37 +0900

    feedRegister add exception if achieveToday true
    Former-commit-id: 08dcc72e897d11143f24aac2794a2b5e44b9f08d

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acbaf65f1377ccabaa31b5b396d041b6cb0604c55b9b55c4b6e4cc63d9015ec5
-size 10629
+oid sha256:5b0b93d264671f64ee3d6a62dbfe7f75bb78cf24fa676867e2d6f1b97d19bde5
+size 10641
