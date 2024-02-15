e091aa51627b83ae3c924adb0ae70de7bfe58c60
좌소연 <thdus981005@naver.com>
2024-02-15 10:55:04 +0900

    update ui
    Former-commit-id: 2a9c74c55403e1248debfea33ccbccd4b7d66723

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccd6c7e3815fcb426c622110ac5465bacfbd42a1cee83baab35099b6af3ea276
-size 4276
+oid sha256:93366d98a8f29643b5e13bcb87b40ec40221627a798b98f67afb4868d85521c2
+size 4442
