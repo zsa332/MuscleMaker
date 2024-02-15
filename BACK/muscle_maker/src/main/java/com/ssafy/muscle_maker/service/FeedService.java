9d7147263b2a5e074b82b50d9b8db37ffbfbb20f
서성원 <zsa332@gmail.com>
2024-02-15 21:38:44 +0900

    Refactor: 피드 조회시 클럽피드 제외
    Former-commit-id: 2cc551b6682cc0ca55e148b7f1ce9303aeb0db94

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5646555a30ead5d08511adef389dc37c35cbb43538ba7cbe66e95c59f85504a
-size 14302
+oid sha256:5f44cb41c29f0fff0b4146501e9849eb39f2300a5b86486e1b951b03674d165c
+size 14470
