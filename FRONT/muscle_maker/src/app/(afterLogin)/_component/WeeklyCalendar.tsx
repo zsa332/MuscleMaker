5ec4bae0246235dcacdcaa270918ba83a9495742
좌소연 <thdus981005@naver.com>
2024-02-15 17:16:06 +0900

    update my page routine page ui
    Former-commit-id: 8c6e7e5ded86a4433fc5a0c6c32212e18d9ed5ce

--- FRONT/muscle_maker/src/app/(afterLogin)/_component/WeeklyCalendar.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/_component/WeeklyCalendar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a55136332c5e779f9dfcacafbf8ec4ce2cb7b5d1567e142df9108704928cc734
-size 10352
+oid sha256:4624d1319646ca0d293412f82ef4917fe0fdc34275700e62949614a463a8a2fb
+size 10713
