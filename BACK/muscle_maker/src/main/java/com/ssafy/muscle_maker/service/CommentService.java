c79c069065bca23b94c982a66dec2d0abf2ecdec
서성원 <zsa332@gmail.com>
2024-02-15 12:49:54 +0900

    Refactor: 본인 피드에 댓글 달경우 알림 x
    Former-commit-id: 95b6a8d54599e903f983950e8309077dddb3b327

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c2393b94f8daa7651552e5f55b8eb00fcbfdfc50a50c478a91cbfaa4a54d810
-size 3199
+oid sha256:586c009fe9ca72a2e97ad55c317b5c7d9f53b39818c0fdfae4a974bf50556ba3
+size 3262
