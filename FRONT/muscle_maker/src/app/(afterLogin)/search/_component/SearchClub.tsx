dcdbd3d13ec06b890573b11f3259dcf7a544d7c3
hello1334 <36934wkd@naver.com>
2024-02-16 01:54:25 +0900

    fix : 리포트 차트 상한선, 하한선 수정, 서치페이지 수정
    Former-commit-id: 4e2ebc07e9418410874cc31705cd1e3a05cc0e71

--- FRONT/muscle_maker/src/app/(afterLogin)/search/_component/SearchClub.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/search/_component/SearchClub.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1f75c3785eba245438a7d1f820f7699a5c7217da506f9c3988a49584f46e9d3
-size 2612
+oid sha256:bb487176e540c966d49dd3cdf3636e90c16df191c7049e00bbc7979e424b005e
+size 2848
