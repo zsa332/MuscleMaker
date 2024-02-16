701a7619a1181809637ade0d112f5a5c9da28c07
Chan <chan@ChanuiMcBookAir.mshome.net>
2024-02-16 09:33:27 +0900

    final
    Former-commit-id: 213ac3e5f51ab9f4da247dd16e0b84eb15ef961e

--- FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/_component/MyExercise.tsx
+++ FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/_component/MyExercise.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b751e1fef65ecd606b1ee6e6d89bbad319016a4bdbbad14fcd19a72cbce686a0
-size 2508
+oid sha256:670c87c22df7cd1204db312842df8732c012be965ec205a0306c7dccfa453064
+size 2626
